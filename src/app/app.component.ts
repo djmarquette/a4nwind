53780a724b68c0dc757745e8836ec217e725ee06
djmarquette <djmarquette@gmail.com>
2017-04-08 22:10:50 -0500

    fixed ngModel error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
-  imports: ['FormsModule']
 })
 export class AppComponent {
   // [] means property binding - Component to DOM
